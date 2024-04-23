abb7522d0240f708e3e952d20ac8e1229de1ca68
yanjiemao <mkdirhao@qq.com>
2024-04-23 23:03:22 +0800

    add image for kitty

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -41,12 +41,12 @@ return {
     },
   },
 
-  -- img preview 
+  -- img preview
   -- but have some mistake in tmux
-  -- it only work in kitty 
+  -- it only work in kitty
   {
     "vhyrro/luarocks.nvim",
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
 
     priority = 1001, -- this plugin needs to run before anything else
@@ -56,26 +56,34 @@ return {
   },
   {
     "3rd/image.nvim",
-    enabled = false,
+    enabled = true,
     event = "VeryLazy",
 
     dependencies = { "luarocks.nvim" },
-    config = function()
-      -- ...
-      require("image").setup({
-        integrations = {
-          markdown = {
-            resolve_image_path = function(document_path, image_path, fallback)
-              -- document_path is the path to the file that contains the image
-              -- image_path is the potentially relative path to the image. for
-              -- markdown it's `![](this text)`
-
-              -- you can call the fallback function to get the default behavior
-              return fallback(document_path, image_path)
-            end,
-          },
+    opts = {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
         },
-      })
-    end,
+        neorg = {
+          enabled = false,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "norg" },
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      tmux_show_only_in_active_window = true,
+      kitty_method = "normal",
+    },
   },
 }
