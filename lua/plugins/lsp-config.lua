b106c1fb01e0f1907518eda22aa91c603f4bd6ee
yanjiemao <mkdirhao@qq.com>
2024-04-30 10:37:14 +0800

    update with tailwindcss

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -38,6 +38,10 @@ return {
 			lspconfig.pyright.setup({
 				capabilities = capabilities,
 			})
+			lspconfig.tailwindcss.setup({
+
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "code show hover" })
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "go define" })
